424204a42cccb42a561e58c9e1696f848dd8abf6
tryit4you <hungitqnam@gmail.com>
2020-11-10 14:02:24 +0700

    update_Textstyle

--- lib/constants/textstyle_constants.dart
+++ lib/constants/textstyle_constants.dart
@@ -7,4 +7,4 @@ var bottomTextStyle=GoogleFonts.inter(fontSize: 12,fontWeight: FontWeight.w500);
 var mTitleStyle=GoogleFonts.inter(fontWeight: FontWeight.w600,color:mTitleColor,fontSize: 16);
 var mMoreDiscountStyle=GoogleFonts.inter(
   fontSize: 12,fontWeight: FontWeight.w700 
-)
+);
